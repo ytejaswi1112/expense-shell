e57ae9339630d7e9e5078bf2f914837f009ad2e7
tejaswi <tejaswi338@gmail.com>
2025-05-04 21:59:12 +0530

    adding mysql

--- mysql.sh
+++ mysql.sh
@@ -2,52 +2,52 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/abc-$TIMESTAMP.log
+SCRIPT_NAME=$(basename "$0" | cut -d "." -f1)
+LOGFILE="/tmp/${SCRIPT_NAME}-${TIMESTAMP}.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-echo "Please enter DB password:"
+
+echo "Please enter MySQL root password:"
 read -s mysql_root_password
 
-VALIDATE(){
-   if [ $1 -ne 0 ]
-   then
+VALIDATE() {
+   if [ $1 -ne 0 ]; then
         echo -e "$2...$R FAILURE $N"
         exit 1
-    else
+   else
         echo -e "$2...$G SUCCESS $N"
-    fi
+   fi
 }
 
-if [ $USERID -ne 0 ]
-then
+if [ "$USERID" -ne 0 ]; then
     echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
+    exit 1
 else
     echo "You are super user."
 fi
 
-
-dnf install mysql-server -y &>>$LOGFILE
+dnf install mysql-server -y &>>"$LOGFILE"
 VALIDATE $? "Installing MySQL Server"
 
-systemctl enable mysqld &>>$LOGFILE
-VALIDATE $? "Enabling MySQL Server"
+systemctl enable mysqld &>>"$LOGFILE"
+VALIDATE $? "Enabling MySQL Service"
 
-systemctl start mysqld &>>$LOGFILE
-VALIDATE $? "Starting MySQL Server"
+systemctl start mysqld &>>"$LOGFILE"
+VALIDATE $? "Starting MySQL Service"
 
-# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-# VALIDATE $? "Setting up root password"
-
-#Below code will be useful for idempotent nature
-mysql -h db.daws78s.online -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
-if [ $? -ne 0 ]
-then
-    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
-    VALIDATE $? "MySQL Root password Setup"
+# Check if root password already works
+mysql -u root -p"${mysql_root_password}" -e "SHOW DATABASES;" &>>"$LOGFILE"
+if [ $? -ne 0 ]; then
+    echo "Setting root password..."
+    temp_pass=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+    
+    mysql --connect-expired-password -u root -p"$temp_pass" <<EOF &>>"$LOGFILE"
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${mysql_root_password}';
+UNINSTALL COMPONENT 'file://component_validate_password';
+EOF
+    VALIDATE $? "MySQL Root Password Setup"
 else
-    echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
-fi  
+    echo -e "MySQL root password is already set...$Y SKIPPING $N"
+fi
