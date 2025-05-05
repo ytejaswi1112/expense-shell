3c70d7a7c3bbc2259a2df2fa6e6bececb50ddc01
tejaswi <tejaswi338@gmail.com>
2025-05-05 11:44:18 +0530

    adding mysql

--- mysql.sh
+++ mysql.sh
@@ -10,15 +10,15 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-echo -e "Please enter MySQL root password you want to set:"
+echo -e "${Y}Please enter MySQL root password you want to set:${N}"
 read -s mysql_root_password
 
 VALIDATE() {
    if [ $1 -ne 0 ]; then
-        echo -e "$2...$R FAILURE $N"
-        exit 1
+      echo -e "$2...$R FAILURE $N"
+      exit 1
    else
-        echo -e "$2...$G SUCCESS $N"
+      echo -e "$2...$G SUCCESS $N"
    fi
 }
 
@@ -26,7 +26,7 @@ if [ "$USERID" -ne 0 ]; then
     echo -e "${R}Please run this script with root access.${N}"
     exit 1
 else
-    echo -e "You are super user."
+    echo -e "${G}You are super user.${N}"
 fi
 
 echo -e "Installing MySQL Server..."
@@ -39,37 +39,43 @@ VALIDATE $? "Enabling MySQL Service"
 systemctl start mysqld &>>"$LOGFILE"
 VALIDATE $? "Starting MySQL Service"
 
-echo -e "Verifying MySQL Service Status..."
-systemctl is-active --quiet mysqld
-VALIDATE $? "MySQL Service Status"
-
-# Try logging in with provided password
+echo -e "Verifying MySQL root password..."
 mysql -u root -p"${mysql_root_password}" -e "SHOW DATABASES;" &>>"$LOGFILE"
 if [ $? -ne 0 ]; then
-    echo -e "$Y MySQL root password doesn't work. Attempting reset... $N"
+    echo -e "${Y}MySQL root password doesn't work. Attempting reset...${N}"
 
-    echo -e "$Y Stopping MySQL for reset... $N"
+    echo -e "Stopping MySQL..."
     systemctl stop mysqld &>>"$LOGFILE"
     VALIDATE $? "Stopping MySQL"
 
-    echo -e "$Y Starting MySQL manually in skip-grant-tables mode... $N"
-    /usr/libexec/mysqld --skip-grant-tables --skip-networking &>>"$LOGFILE" &
+    echo -e "Creating systemd override to use --skip-grant-tables..."
+    mkdir -p /etc/systemd/system/mysqld.service.d
+    cat <<EOF > /etc/systemd/system/mysqld.service.d/override.conf
+[Service]
+ExecStart=
+ExecStart=/usr/libexec/mysqld --skip-grant-tables --skip-networking
+EOF
+
+    systemctl daemon-reexec &>>"$LOGFILE"
+    systemctl daemon-reload &>>"$LOGFILE"
+    systemctl start mysqld &>>"$LOGFILE"
     sleep 5
 
-    echo -e "$Y Resetting root password... $N"
+    echo -e "Resetting MySQL root password..."
     mysql -u root <<EOF &>>"$LOGFILE"
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${mysql_root_password}';
 EOF
     VALIDATE $? "Resetting MySQL root password"
 
-    echo -e "$Y Killing skip-grant-tables process... $N"
-    pkill -f mysqld
-    sleep 5
+    echo -e "Cleaning up systemd override..."
+    rm -f /etc/systemd/system/mysqld.service.d/override.conf
 
-    echo -e "$Y Restarting MySQL normally... $N"
-    systemctl start mysqld &>>"$LOGFILE"
-    VALIDATE $? "Restarting MySQL"
+    systemctl daemon-reexec &>>"$LOGFILE"
+    systemctl daemon-reload &>>"$LOGFILE"
+    systemctl restart mysqld &>>"$LOGFILE"
+    VALIDATE $? "Restarting MySQL with new root password"
+
 else
-    echo -e "MySQL root password is already working...$Y SKIPPING RESET $N"
+    echo -e "MySQL root password is already set...${Y} SKIPPING ${N}"
 fi
